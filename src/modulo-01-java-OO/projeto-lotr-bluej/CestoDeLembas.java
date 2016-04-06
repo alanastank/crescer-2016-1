acd2a88e716beea625c2d64ed28eec535cb10151
Alana Stankiewicz de Souza <alana.souza@cwinet.local>
2016-04-06 14:52:39 -0300

    refactor(CestoDeLembas e CestoDeLembasTest): melhorar performance do código

--- src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembas.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembas.java
@@ -1,25 +1,15 @@
-﻿public class CestoDeLembas
-{
+﻿public class CestoDeLembas {
     private int numLembas;
 
-    public CestoDeLembas(int numLembas)
-    {
+    public CestoDeLembas(int numLembas) {
         this.numLembas = numLembas;
     }
-
     
-    public boolean podeDividirEmPares()
-    {
-
-        if (numLembas >= 1 && numLembas <= 100){
-            if (numLembas % 2 == 0 && numLembas != 2)  {
-                return true;
-           } 
-           else {
-            return false;
-           }
-        } else {
-            System.out.println("Valor inválido.");
-            return false;}
+    public boolean podeDividirEmPares() {
+       // boolean podeDividir = numLembas > 2 && numLembas <= 100 && numLembas % 2 == 0;
+       // return podeDividir ? true : false; // Ternário > avalia se a variavel é verdadeira, se for, retorna true, se não for, retorna false.
+       // OU, mais simples:
+       
+       return numLembas > 2 && numLembas <= 100 && numLembas % 2 == 0;
     }
 }
