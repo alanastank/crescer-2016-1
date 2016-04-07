eb3e061d4f103a71a85a15509ee1797ddf1ca5ac
Alana Stankiewicz de Souza <alana.souza@cwinet.local>
2016-04-07 17:07:36 -0300

    feat(Dwarf, DwarfTest): implementar novas funções em Dwarf e novos testes em DwarfTest

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -1,6 +1,7 @@
 public class Dwarf {
     private int vida;
     private String nome;
+    private Status status = Status.VIVO;
  
     public void setNome(String novoNome){ // usar somente quando for necessário alterar algo
     nome = novoNome;
@@ -13,6 +14,10 @@ public class Dwarf {
     public int getVida(){
     return vida;
     }
+    
+     public Status getStatus(){
+    return status;
+    }
         
     public Dwarf(String nome) {
         this.vida = 110;
@@ -20,7 +25,13 @@ public class Dwarf {
     }
     
     public void levarDano() {
+        if(this.vida > 0) {
         this.vida -= 10;
+       }
+       if(this.vida <= 0){
+        this.status = Status.MORTO;
+        this.vida = 0; // se a vida for negativa, modifica pra 0
+       }
     }
   
 }
--- src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
@@ -5,20 +5,79 @@ import org.junit.Test;
 
 public class DwarfTest {
     @Test
-    public void testaCriarDwarf(){
+    public void testarCriarDwarf(){
+        // Arrange
         Dwarf dwarf = new Dwarf("Atchim");
         int vidaEsperada = 110;
+        // Act
         int vida = dwarf.getVida();
+        // Assert
         assertEquals(vidaEsperada, vida);  
     } 
     
     @Test
-    public void testaLevarDano(){
+    public void testarLevarDano(){
+        // Arrange
         Dwarf dwarf = new Dwarf("Atchim");
         int vidaEsperadaAposDano = 100;
+        // Act
         dwarf.levarDano();
         int vidaAposLevarDano = dwarf.getVida();
+        // Assert
         assertEquals(vidaEsperadaAposDano, vidaAposLevarDano);  
     }     
+    
+    @Test
+    public void testarSeDwarfNasceVivo() {
+        // Arrange
+        Dwarf dwarf = new Dwarf("Balin");
+        Status esperado = Status.VIVO;
+        // Act
+        Status atual = dwarf.getStatus();
+        // Assert
+        assertEquals(esperado, atual);
+    }
+    
+    @Test
+    public void testarMatarDwarf() {
+        // Arrange
+        Dwarf dwarf = new Dwarf("Balin");
+        Status esperado = Status.MORTO;
+        // Act
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        // Assert
+        assertEquals(esperado, dwarf.getStatus());
+    }
+    
+    @Test 
+    public void testarSeAVidaFicaNegativaDepoisDe11Danos(){
+        Dwarf dwarf = new Dwarf("Balin");
+        int vidaEsperada = 0;
+        // Act
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        dwarf.levarDano();
+        // Assert
+        assertEquals(vidaEsperada, dwarf.getVida());
+    }
 }
 
