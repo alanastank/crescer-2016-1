33c1f07edee45877ad47d6102e9ba8b48b21ed31
Alana Stankiewicz <alanass_12@hotmail.com>
2016-04-06 22:43:11 -0300

    feat(DataTerceiraEra): adicionar classe DataTerceiraEra permitindo ve…
    …rificar se um ano é bissexto ou não.

--- src/modulo-01-java-OO/projeto-calendário-de-tolkien-bluej/DataTerceiraEra.java
+++ src/modulo-01-java-OO/projeto-calendário-de-tolkien-bluej/DataTerceiraEra.java
@@ -2,6 +2,8 @@
 1.1 Crie um objeto DataTerceiraEra com 3 campos: dia, mês e ano. Todos campos são inteiros. Faça os métodos getters apenas.
 1.2 Adicione um construtor à classe DataTerceiraEra com os 3 campos existentes no objeto.
 1.3 Adicione um método ehBissexto() no objeto DataTerceiraEra que retorna true caso o ano da data seja bissexto, false caso contrário.
+Obs: Necessita de uma conversão de números do sistema decimal (08, 09) para o 
+sistema Octal (01, 02, 03, 04, 05, 06, 07) para que esses valores possam ser utilizados.
 */
 public class DataTerceiraEra {
     private int dia, mes, ano;
