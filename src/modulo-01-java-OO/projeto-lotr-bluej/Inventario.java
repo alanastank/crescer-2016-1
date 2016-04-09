62a556780295bb997725b4654772664d789a854c
Alana Stankiewicz <Alana Stankiewicz>
2016-04-08 23:13:06 -0300

    feat(Inventario): adicionar método getItemComMaiorQuantidade() e respectivos testes

--- src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
@@ -31,4 +31,20 @@ public class Inventario {
 
         return descricao;
     }
+    
+    public Item getItemComMaiorQuantidade(){
+        int maiorQuantidade = 0;
+        Item itemMaiorQuantidade = new Item(0,"");
+        for(int i=0; i < listaItens.size(); i++){
+            if(listaItens.get(i).getQuantidade() > maiorQuantidade){
+                maiorQuantidade = listaItens.get(i).getQuantidade();
+                itemMaiorQuantidade = listaItens.get(i);
+            }
+        }
+        return itemMaiorQuantidade;
+    }
+    
+    public void ordenarItens(){
+    
+    }
 }
--- src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
@@ -8,45 +8,94 @@ public class InventarioTest {
     
     @Test
     public void adicionaItemaAoInventarioCorretamente() {
+        // Arrange
         Inventario listaItens = new Inventario();
         Item item = new Item(2, "espada");
+        // Act
         listaItens.adicionarItem(item);
+        // Assert
         assertTrue(listaItens.getListaItens().contains(item));
     }
     
     @Test
     public void testaRemoverItem() {
-        Inventario listaItens = new Inventario();
+        // Arrange
+        Inventario inventario = new Inventario();
         Item item = new Item(5, "poção");
-        listaItens.adicionarItem(item);
-        listaItens.removerItem(item);
-        assertFalse(listaItens.getListaItens().contains(item));
+        // Act
+        inventario.adicionarItem(item);
+        inventario.removerItem(item);
+        // Assert
+        assertFalse(inventario.getListaItens().contains(item));
     }
     
     @Test
     public void getDescricoesItensVazios() {
+        // Arrange
         Inventario inventario = new Inventario();
-        String obtido = inventario.getDescricoesItens();
-        assertEquals("", obtido);
+        // Assert
+        assertEquals("", inventario.getDescricoesItens());
     }
     
     @Test
     public void getDescricoesComUmItem() {
+        // Arrange
         Inventario inventario = new Inventario();
-        Item item = new Item(2, "capacete");
+        Item item = new Item(2, "cantil");
+        // Act
         inventario.adicionarItem(item);
-        String obtido = inventario.getDescricoesItens();
-        assertEquals("capacete", obtido);
+        // Assert
+        assertEquals("cantil", inventario.getDescricoesItens());
     }
     
     @Test
     public void getDescricoesComDoisItens() {
+        // Arrange
         Inventario inventario = new Inventario();
-        Item item = new Item(2, "capacete");
-        Item item2 = new Item(5, "espada");
+        Item item = new Item(2, "cantil");
+        Item item2 = new Item(5, "mapa");
+        // Act
         inventario.adicionarItem(item);
         inventario.adicionarItem(item2);
-        String obtido = inventario.getDescricoesItens();
-        assertEquals("capacete,espada", obtido);
+        // Assert
+        assertEquals("cantil,mapa", inventario.getDescricoesItens());
+    }
+    
+    @Test
+    public void getItemComMaiorQuantidadeDeUmaListaVazia(){
+        // Arrange
+        Inventario inventario = new Inventario();
+        // Assert
+        assertEquals(0, inventario.getItemComMaiorQuantidade().getQuantidade());
+    }
+    
+    @Test
+    public void getItemComMaiorQuantidadeDeUmaListaCom1Item(){
+        // Arrange
+        Inventario inventario = new Inventario();
+        Item item = new Item(2, "cantil");
+        // Act
+        inventario.adicionarItem(item);
+        inventario.getItemComMaiorQuantidade();
+        // Assert
+        assertEquals(2, inventario.getItemComMaiorQuantidade().getQuantidade());
+    }
+    
+    @Test
+    public void getItemComMaiorQuantidadeDeUmaListaCom4Itens(){
+        // Arrange
+        Inventario inventario = new Inventario();
+        Item item = new Item(2, "cantil");
+        Item item2 = new Item(5, "moedas de ouro");
+        Item item3 = new Item(8, "martelo");
+        Item item4 = new Item(1, "poção");
+        // Act
+        inventario.adicionarItem(item);
+        inventario.adicionarItem(item2);
+        inventario.adicionarItem(item3);
+        inventario.adicionarItem(item4);
+        inventario.getItemComMaiorQuantidade();
+        // Assert
+        assertEquals(8, inventario.getItemComMaiorQuantidade().getQuantidade());
     }
 }
