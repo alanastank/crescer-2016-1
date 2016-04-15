662d5d7a55abd7c4dfd7ea98e4bd8750be4475a5
Alana Stankiewicz de Souza <alana.souza@cwinet.local>
2016-04-15 16:35:15 -0300

    feat(EstrategiaAtaqueIntercalado, EstrategiaAtaqueIntercaladoTest): adicionar estratégia de ataque intercalado e respectivos testes

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaAtaqueIntercalado.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.*;
+
+/*Crie mais um exército de Elfos que utiliza outra estratégia de ataque:
+Somente elfos vivos atacarão e, para facilitar a vida do general, a composição do pelotão de ataque deve ter obrigatoriamente 50% de Elfos Verdes e 50% de Elfos Noturnos.
+Os ataques ocorrem de forma intercalada e contínua entre os tipos de elfos. Se um Elfo Verde começa atacando, o próximo ataque deve ser feito por um Elfo Noturno.
+Se um Elfo Noturno começa atacando, o próximo ataque deve ser feito por um Elfo Verde. Cada Elfo vai atacar todos os Dwarves da Horda.*/
+
+public class EstrategiaAtaqueIntercalado implements EstrategiaDeAtaque {
+    ArrayList<Elfo> ordemDoUltimoAtaque;
+    
+    public void atacar(ArrayList<Elfo> elfos, ArrayList<Dwarf> dwarves){
+        ordemDoUltimoAtaque = new ArrayList<Elfo>();
+        ArrayList<Elfo> elfosVerdes = new ArrayList<>();
+        ArrayList<Elfo> elfosNoturnos = new ArrayList<>();
+        
+        for(Elfo elfo : elfos){
+            if(elfo instanceof ElfoNoturno){ 
+                elfosNoturnos.add(elfo);
+            } else{
+                elfosVerdes.add(elfo);
+            }
+        }   
+            
+        if(elfosVerdes.size() == elfosNoturnos.size()){           
+            for(int i=0; i < elfosVerdes.size(); i++){
+                for(Dwarf dwarf : dwarves){
+                    elfosVerdes.get(i).atirarFlecha(dwarf);
+                    elfosNoturnos.get(i).atirarFlecha(dwarf);  
+                }
+                ordemDoUltimoAtaque.add(elfosVerdes.get(i));
+                ordemDoUltimoAtaque.add(elfosNoturnos.get(i));                    
+            }           
+        } else{
+            ordemDoUltimoAtaque = null;
+        }
+    }
+        
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque(){
+        return ordemDoUltimoAtaque;
+    }
+}
---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaAtaqueIntercaladoTest.java
@@ -0,0 +1,68 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+public class EstrategiaAtaqueIntercaladoTest {
+    @Test
+   public void seOPelotaoEhCompostoPor50PorCentoDeCadaTipoDeElfoAtacaIntercaladamente(){
+       //Arrang
+        EstrategiaDeAtaque estrategia = new EstrategiaAtaqueIntercalado();
+        ArrayList<Elfo> exercitoElfos = new ArrayList<>();
+        Elfo elfoNoturno1 = new ElfoNoturno("Pelleas");
+        Elfo elfoNoturno2 = new ElfoNoturno("Rennyn");
+        Elfo elfoNoturno3 = new ElfoNoturno("Keya");
+        Elfo elfoVerde1 = new ElfoVerde("Alasse");
+        Elfo elfoVerde2 = new ElfoVerde("Nelaeryn");
+        Elfo elfoVerde3 = new ElfoVerde("Cailu");
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf dwarf1 = new Dwarf("Luirlan");
+        Dwarf dwarf2 = new Dwarf("Cailu");
+       // Act
+        exercitoElfos.add(elfoNoturno1);
+        exercitoElfos.add(elfoNoturno2);
+        exercitoElfos.add(elfoNoturno3);
+        exercitoElfos.add(elfoVerde1);
+        exercitoElfos.add(elfoVerde2);
+        exercitoElfos.add(elfoVerde3);
+        dwarves.add(dwarf1);
+        dwarves.add(dwarf2);
+        estrategia.atacar(exercitoElfos, dwarves);
+        estrategia.getOrdemDoUltimoAtaque();
+       // Assert
+        assertEquals(elfoVerde1, estrategia.getOrdemDoUltimoAtaque().get(0));
+        assertEquals(elfoNoturno1, estrategia.getOrdemDoUltimoAtaque().get(1));
+        assertEquals(elfoVerde2, estrategia.getOrdemDoUltimoAtaque().get(2));
+        assertEquals(elfoNoturno2, estrategia.getOrdemDoUltimoAtaque().get(3));
+        assertEquals(elfoVerde3, estrategia.getOrdemDoUltimoAtaque().get(4));
+        assertEquals(elfoNoturno3, estrategia.getOrdemDoUltimoAtaque().get(5));
+   }
+   
+   @Test
+   public void seOPelotaoNaoEhCompostoPor50PorCentoDeCadaTipoDeElfoNaoAtaca(){
+       //Arrang
+        EstrategiaDeAtaque estrategia = new EstrategiaAtaqueIntercalado();
+        ArrayList<Elfo> exercitoElfos = new ArrayList<>();
+        Elfo elfoNoturno1 = new ElfoNoturno("Pelleas");
+        Elfo elfoNoturno2 = new ElfoNoturno("Rennyn");
+        Elfo elfoNoturno3 = new ElfoNoturno("Keya");
+        Elfo elfoVerde1 = new ElfoVerde("Alasse");
+        Elfo elfoVerde2 = new ElfoVerde("Nelaeryn");
+        ArrayList<Dwarf> dwarves = new ArrayList<>();
+        Dwarf dwarf1 = new Dwarf("Luirlan");
+        Dwarf dwarf2 = new Dwarf("Cailu");
+       // Act
+        exercitoElfos.add(elfoNoturno1);
+        exercitoElfos.add(elfoNoturno2);
+        exercitoElfos.add(elfoNoturno3);
+        exercitoElfos.add(elfoVerde1);
+        exercitoElfos.add(elfoVerde2);
+        dwarves.add(dwarf1);
+        dwarves.add(dwarf2);
+        estrategia.atacar(exercitoElfos, dwarves);
+        estrategia.getOrdemDoUltimoAtaque();
+       // Assert
+        assertEquals(null, estrategia.getOrdemDoUltimoAtaque());
+   }
+}
