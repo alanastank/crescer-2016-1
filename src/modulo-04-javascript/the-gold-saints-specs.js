644fcca47d9db85e4ff04e9e0445b92474e50ba6
Alana Stankiewicz <Alana Stankiewicz>
2016-05-04 12:14:24 -0300

    feat(the-gold-saints-specs.js): adicionar testes

--- src/modulo-04-javascript/the-gold-saints-specs.js
+++ src/modulo-04-javascript/the-gold-saints-specs.js
@@ -17,3 +17,34 @@ describe('Ex 4. Altura média', function() {
     expect(obterAlturaMedia()).toEqual(1.85);
   });
 });
+
+describe('Ex 5. Altura mediana', function() {
+  it('retorna 1.85', function() {
+    expect(obterAlturaMediana()).toEqual(1.85);
+  });
+});
+
+describe('Ex 6A. Peso médio', function() {
+  it('retorna 84.27', function() {
+    expect(obterPesoMedio()).toEqual(84.27);
+  });
+});
+
+describe('Ex 6B. Peso médio dos doadores', function() {
+  it('retorna 80.67', function() {
+    expect(obterPesoMedioDoadores()).toEqual(80.67);
+  });
+});
+
+describe('Ex 7. IMC', function() {
+  it('retorna [22.64, 29.48, 24.62, 24.22, 24.84, 20.53, 24.54, 24.31, 23.99, 22.45, 21.5]', function() {
+    expect(obterIMC()).toEqual([22.64, 29.48, 24.62, 24.22, 24.84, 20.53, 24.54, 24.31, 23.99, 22.45, 21.5]);
+  });
+});
+
+// está errado, tem que arrumar
+describe('Ex 8. Sobrepeso', function() {
+  it('retorna cavaleiro de id 2', function() {
+    expect(obterSobrepeso()).toEqual(goldSaints[1]);
+  });
+});
