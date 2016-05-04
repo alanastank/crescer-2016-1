4dc8a9776c6d14f8a0439a895952dda49327efed
Alana Stankiewicz de Souza <alana.souza@cwinet.local>
2016-05-04 15:16:22 -0300

    feat(the-gols-saints.js): adicionar correções instrutor e manter resoluções iniciais comentadas

--- src/modulo-04-javascript/the-gold-saints.js
+++ src/modulo-04-javascript/the-gold-saints.js
@@ -4,114 +4,201 @@ var goldSaints = JSON.parse(
   '[{"id":1,"nome":"Mu","dataNascimento":"1967-03-27T03:00:00.000Z","alturaCm":182,"pesoLb":165.35,"signo":"\u00c1ries","tipoSanguineo":"A","localNascimento":"Tibete","localTreinamento":"Jamiel","golpes":["Parede de Cristal","Extin\u00e7\u00e3o Estelar","Revolu\u00e7\u00e3o Estelar"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900361\/93eed452-0d66-11e6-8ba2-e083b8297b2b.png","isThumb":true}]},{"id":2,"nome":"Aldebaran","dataNascimento":"1967-05-08T03:00:00.000Z","alturaCm":210,"pesoLb":286.600941,"signo":"Touro","tipoSanguineo":"B","localNascimento":"Brasil","localTreinamento":"Brasil","golpes":["Grande Chifre"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900419\/dca83616-0d66-11e6-9757-8d07311e6999.png","isThumb":true}]},{"id":3,"nome":"Saga","dataNascimento":"1959-05-30T03:00:00.000Z","alturaCm":188,"pesoLb":191.81,"signo":"G\u00eameos","tipoSanguineo":"AB","localNascimento":"Gr\u00e9cia","localTreinamento":"Santu\u00e1rio, Gr\u00e9cia","golpes":["Sat\u00e3 Imperial","Outra Dimens\u00e3o","Explos\u00e3o Gal\u00e1ctica"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900456\/00e25b7e-0d67-11e6-9fb3-4b35577080d2.png","isThumb":true}]},{"id":4,"nome":"M\u00e1scara da Morte","dataNascimento":"1964-06-24T03:00:00.000Z","alturaCm":184,"pesoLb":180.77900,"signo":"C\u00e2ncer","tipoSanguineo":"A","localNascimento":"It\u00e1lia","localTreinamento":"Sic\u00edlia, It\u00e1lia","golpes":["Ondas do Inferno"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900489\/2cc0fd40-0d67-11e6-9bc0-600c5381c650.png","isThumb":true}]},{"id":5,"nome":"Aiolia","dataNascimento":"1967-08-16T03:00:00.000Z","alturaCm":185,"pesoLb":187.392923,"signo":"Le\u00e3o","tipoSanguineo":"O","localNascimento":"Gr\u00e9cia","localTreinamento":"Santu\u00e1rio, Gr\u00e9cia","golpes":["C\u00e1psula do Poder","Pata do Le\u00e3o","Rel\u00e2mpago de Plasma"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900613\/c4ba42f0-0d67-11e6-9c0e-e79c2278ab0b.png","isThumb":true}]},{"id":6,"nome":"Shaka","dataNascimento":"1967-09-19T03:00:00.000Z","alturaCm":182,"pesoLb":149.914338,"signo":"Virgem","tipoSanguineo":"AB","localNascimento":"\u00cdndia","localTreinamento":"Ganges, \u00cdndia","golpes":["Rendi\u00e7\u00e3o Divina","Ciclo das 6 Exist\u00eancias","Tesouro do C\u00e9u","Invoca\u00e7\u00e3o dos Esp\u00edritos","Ohm!","Kahn!"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900695\/4a5675dc-0d68-11e6-8396-2a775a2b0c39.png","isThumb":true}]},{"id":7,"nome":"Dohko","dataNascimento":"1726-10-20T03:00:00.000Z","alturaCm":170,"signo":"Libra","tipoSanguineo":"A","localNascimento":"China","localTreinamento":"5 Picos Antigos de Rozan, China","golpes":["C\u00f3lera do Drag\u00e3o","C\u00f3lera dos 100 Drag\u00f5es"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900848\/ea27d2e0-0d68-11e6-9d73-78add86a1811.png","isThumb":true}]},{"id":8,"nome":"Milo","dataNascimento":"1967-11-08T03:00:00.000Z","alturaCm":185,"pesoLb":185.1883,"signo":"Escorpi\u00e3o","tipoSanguineo":"B","localNascimento":"Gr\u00e9cia","localTreinamento":"Ilha de Milos, Gr\u00e9cia","golpes":["Restri\u00e7\u00e3o","Agulha Escarlate","Antares"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14900894\/36445900-0d69-11e6-81c9-77cb17448b9d.png","isThumb":true}]},{"id":9,"nome":"Aiolos","dataNascimento":"1960-11-30T03:00:00.000Z","alturaCm":187,"pesoLb":187.392923,"signo":"Sagit\u00e1rio","tipoSanguineo":"O","localNascimento":"Gr\u00e9cia","localTreinamento":"Santu\u00e1rio, Gr\u00e9cia","golpes":["Trov\u00e3o At\u00f4mico","Flecha da Justi\u00e7a"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14901061\/e5fe3b90-0d69-11e6-9a78-2449055be1fa.png","isThumb":true}]},{"id":10,"nome":"Shura","dataNascimento":"1964-01-12T03:00:00.000Z","alturaCm":186,"pesoLb":182.983678,"signo":"Capric\u00f3rnio","tipoSanguineo":"B","localNascimento":"Espanha","localTreinamento":"Montes Pirineus, Espanha","golpes":["Excalibur"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14901144\/5af186c8-0d6a-11e6-934a-35db18b16752.png","isThumb":true}]},{"id":11,"nome":"Camus","dataNascimento":"1967-02-07T03:00:00.000Z","alturaCm":184,"pesoLb":167.551319,"signo":"Aqu\u00e1rio","tipoSanguineo":"A","localNascimento":"Fran\u00e7a","localTreinamento":"Sib\u00e9ria Oriental","golpes":["P\u00f3 de Diamante","Trov\u00e3o Aurora","Execu\u00e7\u00e3o Aurora"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14901196\/9f128f78-0d6a-11e6-86b3-1a68234e9bfc.png","isThumb":true}]},{"id":12,"nome":"Afrodite","dataNascimento":"1965-03-10T03:00:00.000Z","alturaCm":183,"pesoLb":158.732829,"signo":"Peixes","tipoSanguineo":"O","localNascimento":"Su\u00e9cia","localTreinamento":"Groel\u00e2ndia","golpes":["Rosas Diab\u00f3licas Reais","Rosas Piranhas","Rosa Branca"],"imagens":[{"url":"https:\/\/cloud.githubusercontent.com\/assets\/526075\/14901259\/f4a0b3ca-0d6a-11e6-89b1-59855cabc43d.png","isThumb":true}]}]');
   console.log(goldSaints);
 
+/* Funções utilitárias para re-aproveitar código */
+
+var formatarNumero = function(n) {
+  return Math.round(n * 100) / 100;
+};
+
+var cmParaMetros = function(cm) {
+  return cm / 100;
+};
+
+var lbParaKilos = function(lb) {
+  return lb / 2.20462262;
+};
+
+var apenasComPesoDefinido = function(e) {
+  return typeof e.pesoLb !== 'undefined';
+};
+
+/* Fim de funções utilitárias para aproveitar código */
+
   /* 1 - Escreva uma função obterDoadores que retorne o array com os cavaleiros que são doadores universais. Ignore o fator Rh do sangue.*/
 
-  function obterDoadores(){
-    var doadoresUniversais = [];
-    for (var i in goldSaints){
-      if(goldSaints[i].tipoSanguineo === 'O'){
-        doadoresUniversais.push(goldSaints[i]);
-      }
-    }
-    return doadoresUniversais;
-  }
+// function obterDoadores(){
+//   var doadoresUniversais = [];
+//   for (var i in goldSaints){
+//     if(goldSaints[i].tipoSanguineo === 'O'){
+//       doadoresUniversais.push(goldSaints[i]);
+//     }
+//   }
+//   return doadoresUniversais;
+// }
+
+function obterDoadores(){
+    return goldSaints.filter(function(e) {
+    return e.tipoSanguineo === 'O';
+  });
+};
 
 /* 2 - Escreva uma função obterCavaleiroComMaisGolpes que retorne o cavaleiro com o maior número de golpes.*/
 
-function obterCavaleiroComMaisGolpes(){
-  var cavaleiroComMaisGolpes = goldSaints[0];
-  for (var i in goldSaints){
-    if(cavaleiroComMaisGolpes.golpes.length < goldSaints[i].golpes.length){
-      cavaleiroComMaisGolpes = goldSaints[i];
-    }
-  }
-  return cavaleiroComMaisGolpes;
-}
+// function obterCavaleiroComMaisGolpes(){
+//   var cavaleiroComMaisGolpes = goldSaints[0];
+//   for (var i in goldSaints){
+//     if(cavaleiroComMaisGolpes.golpes.length < goldSaints[i].golpes.length){
+//       cavaleiroComMaisGolpes = goldSaints[i];
+//     }
+//   }
+//   return cavaleiroComMaisGolpes;
+// }
+
+function obterCavaleiroComMaisGolpes() {
+  return goldSaints.concat().sort(function(a, b) {
+    return b.golpes.length - a.golpes.length;
+    //return a.golpes.length < b.golpes.length ? 1 : -1;
+  })[0];
+};
 
 /* 3 - Escreva uma função obterMesesComMaisAniversarios que retorne o array com os meses do ano que têm o maior número de cavaleiros aniversariantes. Ex: obterMesesComMaisAniversarios(); => [ 'Janeiro' ]*/
 
 /* 4 - Escreva uma função obterAlturaMedia que retorna a altura média entre os cavaleiros de ouro.
 IMPORTANTE: a altura deve ser retornada em metros e de forma numérica.*/
 
+// function obterAlturaMedia(){
+//   var soma = 0;
+//   for(var i in goldSaints){
+//     soma += (goldSaints[i].alturaCm / 100);
+//   }
+//   return parseFloat((soma / 12).toFixed(2));
+// }
+
 function obterAlturaMedia(){
-  var soma = 0;
-  for(var i in goldSaints){
-    soma += (goldSaints[i].alturaCm / 100);
-  }
-  return parseFloat((soma / 12).toFixed(2));
+  var media = (goldSaints.reduce(function(acumulador, alturaAtual) {
+    return acumulador + alturaAtual.alturaCm;
+  }, 0)) / goldSaints.length;
+  return formatarNumero(cmParaMetros(media));
 }
 
 /* 5 - Escreva uma função obterAlturaMediana que retorna a altura mediana dos cavaleiros de ouro.
 IMPORTANTE: a altura deve ser retornada em metros e de forma numérica.*/
 
+// function obterAlturaMediana() {
+//   var alturas = [];
+//   for(var i in goldSaints){
+//     alturas.push(goldSaints[i].alturaCm/100);
+//   }
+//   alturas.sort();
+//   var metade = (alturas.length/2);
+//   if(alturas.length % 2 !== 0){
+//     return alturas[metade];
+//   }
+//   else {
+//     return formatarNumero((alturas[metade-1] + alturas[metade]) /2);
+//   }
+// }
+
 function obterAlturaMediana() {
-  var alturas = [];
-  for(var i in goldSaints){
-    alturas.push(goldSaints[i].alturaCm/100);
-  }
-  alturas.sort();
-  var metade = (alturas.length/2);
-  if(alturas.length % 2 !== 0){
-    return alturas[metade];
-  }
-  else {
-    return parseFloat(((alturas[metade-1] + alturas[metade]) /2).toFixed(2));
-  }
-}
+  // Mediana de um conjunto par é a média dos elementos centrais
+  var goldSaintsOrdenados = goldSaints.concat().sort(function(a, b) {
+    return a.alturaCm - b.alturaCm;
+  });
+  var elementoCentral = goldSaintsOrdenados.length / 2;
+  var medianaAltura = (goldSaintsOrdenados[elementoCentral].alturaCm + goldSaintsOrdenados[elementoCentral - 1].alturaCm) / 2;
+  return formatarNumero(cmParaMetros(medianaAltura));
+};
 
 /* 6 A - Escreva uma função obterPesoMedio que retorna o peso médio em kilos dos cavaleiros de ouro.
      B - Escreva uma função obterPesoMedioDoadores que retorna o peso médio em kilos dos cavaleiros que são doadores.*/
 
-function obterPesoMedio(){
-  var soma = 0;
-  var contador = 0;
-  for(var i in goldSaints){
-    if(typeof goldSaints[i].pesoLb === 'number'){
-    contador++;
-    soma += (goldSaints[i].pesoLb * 0.453592);
-    }
-  }
-  return parseFloat((soma / contador).toFixed(2));
-}
+// function obterPesoMedio(doadores){
+//   var soma = 0;
+//   var contador = 0;
+//   for(var i in goldSaints){
+//     if(typeof goldSaints[i].pesoLb === 'number'){
+//     contador++;
+//     soma += (goldSaints[i].pesoLb);
+//     }
+//   }
+//   return formatarNumero(lbParaKilos(soma / contador));
+// }
+//
+// function obterPesoMedioDoadores(){
+//   var soma = 0;
+//   var doadoresComPeso = [];
+//   for(var i in goldSaints){
+//     if(goldSaints[i].tipoSanguineo === 'O' && typeof goldSaints[i].pesoLb === 'number'){
+//       doadoresComPeso.push(goldSaints[i]);
+//     }
+//   }
+//   for(var i in doadoresComPeso){
+//     soma += (doadoresComPeso[i].pesoLb);
+//   }
+//   return formatarNumero(lbParaKilos(soma / doadoresComPeso.length));
+// }
 
-function obterPesoMedioDoadores(){
-  var soma = 0;
-  var doadoresComPeso = [];
-  for(var i in goldSaints){
-    if(goldSaints[i].tipoSanguineo === 'O' && typeof goldSaints[i].pesoLb === 'number'){
-      doadoresComPeso.push(goldSaints[i]);
-    }
-  }
-  for(var i in doadoresComPeso){
-    soma += (doadoresComPeso[i].pesoLb * 0.453592);
-  }
-  return parseFloat((soma / doadoresComPeso.length).toFixed(2));
-}
+function obterPesoMedio(cavaleiros) {
+  var cavaleirosComPeso = 0;
+  var mediaPeso = (cavaleiros || goldSaints)
+    .filter(apenasComPesoDefinido)
+    .map(function(e) {
+      cavaleirosComPeso++;
+      return e.pesoLb;
+    })
+    .reduce(function(acc, e) {
+      return acc + e;
+    }, 0) / cavaleirosComPeso;
+
+  return formatarNumero(lbParaKilos(mediaPeso));
+};
+
+function obterPesoMedioDoadores() {
+  var doadores = obterDoadores();
+  return obterPesoMedio(doadores);
+};
 
 /* 7 - Escreva uma função obterIMC que retorna um array com todos índices de massa corporal (IMC) dos cavaleiros com peso registrado.*/
 
-function obterIMC(){
-  var indicesMassaCorporal = [];
-  var calculo = 0;
-  for(var i in goldSaints){
-    if(typeof goldSaints[i].pesoLb === 'number'){
-      calculo = (goldSaints[i].pesoLb * 0.453592) / Math.pow((goldSaints[i].alturaCm / 100),2);
-      indicesMassaCorporal.push(parseFloat((calculo).toFixed(2)));
-    }
-  }
-  return indicesMassaCorporal;
-}
+// function obterIMC(){
+//   var indicesMassaCorporal = [];
+//   var calculo = 0;
+//   for(var i in goldSaints){
+//     if(typeof goldSaints[i].pesoLb === 'number'){
+//       calculo = (goldSaints[i].pesoLb * 0.453592) / Math.pow((goldSaints[i].alturaCm / 100),2);
+//       indicesMassaCorporal.push(parseFloat((calculo).toFixed(2)));
+//     }
+//   }
+//   return indicesMassaCorporal;
+// }
+
+function obterIMC() {
+  return goldSaints
+    .filter(apenasComPesoDefinido)
+    .map(function(e) {
+      var imc = lbParaKilos(e.pesoLb) / Math.pow(cmParaMetros(e.alturaCm), 2);
+      return formatarNumero(imc);
+    });
+};
 
 /* 8 - Escreva uma função obterSobrepeso que retorna um array com os cavaleiros que estão acima de peso.*/
 
-function obterSobrepeso(){
-  var cavaleirosComSobrepeso = [];
-  var calculoIMC = 0;
-  for(var i in goldSaints){
-    calculoIMC = (goldSaints[i].pesoLb * 0.453592) / Math.pow((goldSaints[i].alturaCm / 100),2)
-    if(typeof goldSaints[i].pesoLb === 'number' && calculoIMC >= 25 && calculoIMC <= 29.9){
-        cavaleirosComSobrepeso.push(goldSaints[i]);
-    }
-  }
-  return cavaleirosComSobrepeso;
-}
+// function obterSobrepeso(){
+//   var cavaleirosComSobrepeso = [];
+//   var calculoIMC = 0;
+//   for(var i in goldSaints){
+//     calculoIMC = (goldSaints[i].pesoLb * 0.453592) / Math.pow((goldSaints[i].alturaCm / 100),2)
+//     if(typeof goldSaints[i].pesoLb === 'number' && calculoIMC >= 25 && calculoIMC <= 29.9){
+//         cavaleirosComSobrepeso.push(goldSaints[i]);
+//     }
+//   }
+//   return cavaleirosComSobrepeso;
+// }
+
+function obterSobrepeso() {
+  var imcs = obterIMC();
+  return goldSaints.filter(function(e, indice) {
+    var imc = imcs[indice];
+    return 25 <= imc && imc < 30;
+  });
+};
