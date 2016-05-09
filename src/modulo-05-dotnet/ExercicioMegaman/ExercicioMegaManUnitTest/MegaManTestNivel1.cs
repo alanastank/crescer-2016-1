921a014606a4628caa7a4fdb69ccea92ff7d70b4
Alana Stankiewicz <Alana Stankiewicz>
2016-05-08 21:32:56 -0300

    feat(Pasta de testes, MegaManTestNivel1.cs): adicionar pasta de testes e testes do nível 1

---
+++ src/modulo-05-dotnet/ExercicioMegaman/ExercicioMegaManUnitTest/MegaManTestNivel1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExercicioMegaman;
+
+namespace MegaManUnitTest
+{
+    [TestClass]
+    public class MegaManTestNivel1
+    {
+        [TestMethod]
+        public void seMegamanTemAtaqueInicialIgualA6()
+        {
+            // Arrange
+            var megaman = new Megaman();
+            var bot = new Bot();
+            // Act
+            megaman.Atacar(bot);
+            // Assert
+            Assert.AreEqual(94, bot.Vida);
+        }
+
+        [TestMethod]
+        public void seProtomanTemDefesaIgualA2()
+        {
+            // Arrange
+            var protoman = new Protoman();
+            var megaman = new Megaman();
+            // Act
+            megaman.Atacar(protoman);
+            // Assert
+            Assert.AreEqual(96, protoman.Vida);
+        }
+
+        [TestMethod]
+        public void seAoChamarOMetodoToStringRetornaCorretamente()
+        {
+            // Arrange
+            var bot = new Bot();
+            var esperado = "Nome: Bot, Vida: 100, Ataque: 5, Defesa: 0";
+            // Assert
+            Assert.AreEqual(esperado, bot.ToString());
+        }
+
+        [TestMethod]
+        public void seProtomanFicaCom20DeVidaECom7DeAtaqueAoMorrerPelaPrimeiraVez()
+        {
+            // Arrange
+            var protoman = new Protoman();
+            var esperado = "Nome: Protoman, Vida: 20, Ataque: 7, Defesa: 2";
+            // Act
+            protoman.ReceberDano(102);
+            // Assert
+            Assert.AreEqual(esperado, protoman.ToString());
+        }
+
+        [TestMethod]
+        public void seProtomanFicaCom0DeVidaAoMorrerPelaSegundaVez()
+        {
+            // Arrange
+            var protoman = new Protoman();
+            // Act
+            protoman.ReceberDano(102);
+            // Matar protoman pela segunda vez.
+            protoman.ReceberDano(22);
+            // Assert
+            Assert.AreEqual(0, protoman.Vida);
+        }
+
+        [TestMethod]
+        public void seOAtaqueDoMegamanEh9QuandoSuaVidaEhMenorQue30()
+        {
+            // Arrange
+            var megaman = new Megaman();
+            var esperado = "Nome: Megaman, Vida: 25, Ataque: 9, Defesa: 0";
+            // Act
+            // vida = 25
+            megaman.ReceberDano(71);
+            // Assert
+            Assert.AreEqual(esperado, megaman.ToString());
+        }
+    }
+}
