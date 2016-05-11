2a854b8b5bfcd698b50f0f16d8adadb6a9151229
Alana Stankiewicz <Alana Stankiewicz>
2016-05-11 12:48:20 -0300

    feat(Exercicio Aula 3): adicionar exercicio de MVC

---
+++ src/modulo-05-dotnet/MVC/MVC/Controllers/StreetFighterController.cs
@@ -0,0 +1,37 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class StreetFighterController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FichaTecnica()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            var alana = new List<SobreMimModel>()
+                {
+                    new SobreMimModel()
+                    {
+                        Nome = "Alana",
+                        Idade = 18,
+                        Origem = "São Leopoldo, RS - Brasil",
+                        Escolaridade = "Técnico em Informática Integrado ao Ensino Médio (IFSUL)",
+                        Contato = "alanastankiewicz@gmail.com"
+                    }
+                };
+            return View();
+        }
+    }
+}
---
+++ src/modulo-05-dotnet/MVC/MVC/Models/SobreMimModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SobreMimModel
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Origem { get; set; }
+        public string Escolaridade { get; set; }
+        public string Contato { get; set; }
+    }
+}
