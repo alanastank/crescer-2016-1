e387da036749c8a7f51291a6622040fcbc01c6d3
Alana Stankiewicz <Alana Stankiewicz>
2016-05-15 01:22:52 -0300

    feat(Pedido): adicionar display names

--- src/modulo-05-dotnet/CameloNinja/CameloNinjaa.Dominio/Pedido.cs
+++ src/modulo-05-dotnet/CameloNinja/CameloNinjaa.Dominio/Pedido.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,19 +8,9 @@ namespace CameloNinja.Dominio
 {
     public class Pedido
     {
-
-        public int Id { get; private set; }
-        public DateTime DataPedido { get; private set; }
-        public DateTime DataEntregaDesejada { get; private set; }
-        public string NomeProduto { get; private set; }
-        public decimal Valor { get; private set; }
-        public TipoPagamento TipoPagamento { get; private set; }
-        public string NomeCliente { get; private set; }
-        public string Cidade { get; private set; }
-        public string Estado { get; private set; }
-        public bool PedidoUrgente { get; private set; }
-
+        /// <summary>
         /// Construtor utilizado para montar pedidos novos
+        /// </summary>
         public Pedido(DateTime dataEntregaDesejada, string nomeProduto, decimal valor, TipoPagamento tipoPagamento, string nomeCliente, string cidade, string estado)
         {
             DataEntregaDesejada = dataEntregaDesejada;
@@ -29,13 +20,16 @@ namespace CameloNinja.Dominio
             NomeCliente = nomeCliente;
             Cidade = cidade;
             Estado = estado;
-            DataPedido = DateTime.Today;
-            var diasRestantesParaConcluirEntrega = (dataEntregaDesejada - DataPedido).TotalDays;
+            DataPedido = DateTime.Now;
+
+            var diasRestantesParaConcluirEntrega = (dataEntregaDesejada - DateTime.Today).TotalDays;
             ValidaPossibilidadeEntrega(diasRestantesParaConcluirEntrega);
             DefineUrgenciaDoPedido(diasRestantesParaConcluirEntrega);
         }
 
+        /// <summary>
         /// Construtor utilizado para montar pedidos recuperados do repositório
+        /// </summary>
         public Pedido(int id, DateTime dataPedido, DateTime dataEntregaDesejada, string nomeProduto, decimal valor, TipoPagamento tipoPagamento, string nomeCliente, string cidade, string estado, bool pedidoUrgente)
         {
             Id = id;
@@ -50,6 +44,31 @@ namespace CameloNinja.Dominio
             PedidoUrgente = pedidoUrgente;
         }
 
+        public int Id { get; private set; }
+
+        [Display(Name = "Data do pedido")]
+        public DateTime DataPedido { get; private set; }
+
+        [Display(Name = "Data de entrega desejada")]
+        public DateTime DataEntregaDesejada { get; private set; }
+
+        [Display(Name = "Nome do Produto")]
+        public string NomeProduto { get; private set; }
+   
+        public decimal Valor { get; private set; }
+
+        [Display(Name = "Tipo de Pagamento")]
+        public TipoPagamento TipoPagamento { get; private set; }
+
+        [Display(Name = "Nome do cliente")]
+        public string NomeCliente { get; private set; }
+
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+
+        [Display(Name = "Pedido Urgente")]
+        public bool PedidoUrgente { get; private set; }
+
         private void ValidaPossibilidadeEntrega(double diasRestantesParaConcluirEntrega)
         {
             if (diasRestantesParaConcluirEntrega < 1)
@@ -59,6 +78,14 @@ namespace CameloNinja.Dominio
         private void DefineUrgenciaDoPedido(double diasRestantesParaConcluirEntrega)
         {
             PedidoUrgente = diasRestantesParaConcluirEntrega < 7;
+        }
+
+        public void AtualizarId(int id)
+        {
+            if (Id != 0)
+                throw new InvalidOperationException("Esse objeto já possuia Id, portanto ele já havia sido salvo no banco. Não é possível alterar esse valor.");
+
+            Id = id;
         }
     }
 }
