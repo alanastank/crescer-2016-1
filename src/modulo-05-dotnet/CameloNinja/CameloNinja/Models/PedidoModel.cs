516b8123aed853021526b851b7cff06ec935f435
Alana Stankiewicz <Alana Stankiewicz>
2016-05-15 01:16:34 -0300

    feat(PedidoModel): adicionar id, required error messages e display names

--- src/modulo-05-dotnet/CameloNinja/CameloNinja/Models/PedidoModel.cs
+++ src/modulo-05-dotnet/CameloNinja/CameloNinja/Models/PedidoModel.cs
@@ -1,6 +1,8 @@
 ﻿using CameloNinja.Dominio;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +10,33 @@ namespace CameloNinja.MVC.Models
 {
     public class PedidoModel
     {
+        public int? Id { get; set; }
+
+        [Display(Name = "Data de entrega: ")]
+        [Required(ErrorMessage = "Data desejada de entrega deve ser informada")]
         public DateTime DataEntrega { get; set; }
+
+        [Required(ErrorMessage = "Nome do Produto deve ser informado")]
+        [DisplayName("Nome do Produto")]
         public string NomeProduto { get; set; }
+
+        [Required(ErrorMessage = "Valor de venda deve ser informado")]
+        [DisplayName("Valor de Venda")]
+        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = true)]
         public decimal Valor { get; set; }
+
+        [Required]
+        [DisplayName("Tipo de Pagamento")]
         public TipoPagamento TipoPagamento { get; set; }
+
+        [Required(ErrorMessage = "Nome do Cliente deve ser informado")]
+        [DisplayName("Nome do Cliente")]
         public string NomeCliente { get; set; }
+
+        [Required(ErrorMessage = "Cidade deve ser informada")]
         public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Estado deve ser informado")]
         public string Estado { get; set; }
     }
 }
