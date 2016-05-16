7534a60410c1428dc2a0408d2e5f53eb9ccae1f7
Alana Stankiewicz de Souza <alana.souza@cwinet.local>
2016-05-16 16:51:28 -0300

    feat(Aula6): adicionar Exercicio Aula dia 16/05 fazendo login

---
+++ src/modulo-05-dotnet/Aula 6 - Autenticação e Segurança/Repositorio/UsuarioRepositorioADO.cs
@@ -0,0 +1,42 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Configuration;
+
+namespace Repositorio
+{
+    public class UsuarioRepositorioADO : IUsuarioRepositorio
+    {
+        public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Aula6"].ConnectionString;
+
+            using (var conexao = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT * FROM Usuarios WHERE email=@p_email and senha=@p_senha";
+                Usuario usuario = new Usuario();
+
+                var comando = new SqlCommand(sql, conexao);
+                comando.Parameters.Add(new SqlParameter("p_email", email));
+                comando.Parameters.Add(new SqlParameter("p_senha", senha));
+
+                conexao.Open();
+
+                SqlDataReader leitor = comando.ExecuteReader();
+
+                while (leitor.Read())
+                {
+                    usuario.Email = leitor["email"].ToString();
+                    usuario.Senha = leitor["senha"].ToString();
+                    usuario.Nome = leitor["nome"].ToString();
+                }
+                return usuario;
+            }
+        }
+    }
+}
