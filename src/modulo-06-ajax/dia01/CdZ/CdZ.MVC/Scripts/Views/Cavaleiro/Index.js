8895adab5c521c9eb10cf52c105dba7ca19064b6
Alana Stankiewicz de Souza <alana.souza@cwinet.local>
2016-05-19 16:02:33 -0300

    feat(AJAX): adicionar exemplo de get com ajax

---
+++ src/modulo-06-ajax/dia01/CdZ/CdZ.MVC/Scripts/Views/Cavaleiro/Index.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+$.ajax({ url: urlCavaleiroGet, type: 'GET' })
+.done(function (res) {
+    console.log(res.data);
+    var $cavaleiros = $('#cavaleiros');
+    res.data.forEach(function (cava) {
+        $cavaleiros.append(
+            $('<li>').append(cava.Nome)
+        );
+    });
+});
