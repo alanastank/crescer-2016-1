9d4e9cadbbf96b7f9ca2c0709fee7e907d5968a9
Alana Stankiewicz <Alana Stankiewicz>
2016-05-22 20:33:44 -0300

    feat(CadastroDeCavaleiros.js): adicionar campos para imagens e golpes

---
+++ src/modulo-06-ajax/dia02/CdZ/src/CdZ.MVC/Scripts/Views/CadastroDeCavaleiros.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+$(function () {
+
+    // Adicionar campos para imagens e golpes
+
+    $('#btnAdicionarImg').click(function () {
+        $('#novasImagens').append(gerarElementoLiComInputs());
+    });
+
+    $('#btnAdicionarGolpe').click(function () {
+        $('#novosGolpes').append(gerarElementoLiComInputTexto());
+    });
+
+    function gerarElementoLiComInputs() {
+        var $novoTxt = $('<input>').attr('name', 'urlImagem').attr('type', 'text').attr('placeholder', 'Ex: bit.ly/shiryu.png').addClass('form-control');
+        var $novoCheckbox =
+          $('<label>').append(
+            $('<input>')
+            .attr('type', 'checkbox')
+            .attr('name', 'isThumb')
+            .attr('checked', 'checked')
+          ).append('É thumbnail?');
+        return $('<li>').append($novoTxt).append($novoCheckbox);
+    };
+
+    function gerarElementoLiComInputTexto() {
+        var $novoTxt = $('<input>').attr('name', 'golpe').attr('type', 'text').attr('placeholder', 'Ex: Pó de diamante').addClass('form-control');
+        return $('<li>').append($novoTxt);
+    };
+
+    // Adicionar datepicker
+
+    $('#txtDtNascimento').datepicker({
+        dateFormat: 'dd/mm/yy',
+        changeYear: true,
+        yearRange: "1900:2016"
+    });
+
+});
