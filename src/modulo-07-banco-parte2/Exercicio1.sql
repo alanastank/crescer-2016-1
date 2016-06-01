7d9822c645c3a977624509604ee003ce6359c8ac
Alana Stankiewicz <alanastankiewicz@gmail.com>
2016-06-01 16:05:38 -0300

    feat(exercicio1): adionar 5 e 6

--- src/modulo-07-banco-parte2/Exercicio1.sql
+++ src/modulo-07-banco-parte2/Exercicio1.sql
@@ -6,7 +6,7 @@ Select Count(IDPedido) "Número de pedidos" from Pedido where
 EXTRACT(month FROM TO_DATE(DataPedido, 'dd/mm/yyyy')) = 05 and EXTRACT(year FROM TO_DATE(DataPedido, 'dd/mm/yyyy')) = 16;
 
 -- 3) Liste todos os clientes que tenham o LTDA no nome ou razao social. 
-Select * from Cliente where Nome like '%Ltda%' or RazaoSocial like '%Ltda%';
+Select * from Cliente where lower(Nome) like '%ltda%' or lower(RazaoSocial) like '%ltda%';
 
 -- 4) Crie (insira) um novo registro na tabela de Produto, com as seguintes informações: 
 -- Nome: Galocha Maragato; Preço de custo: 35.67; Preço de venda: 77.95; Situação: A 
@@ -18,10 +18,16 @@ Values (produto_sequence.nextVal, 'Galocha Maragato', '01/06/16', 35.67, 77.95, 
 -- 5) Identifique e liste os produtos que não tiveram nenhum pedido, considere os relacionamentos no modelo de dados, pois não há relacionamento 
 -- direto entre Produto e Pedido (será preciso relacionar PedidoItem). Obs.: o produto criado anteriormente deverá ser listado (apenas este)
 
+Select * from Produto PR FULL OUTER JOIN PedidoItem PI on PR.IdProduto = PI.IdProduto where PI.IdProduto is null;
 
 -- 6) Liste todos os pedidos de um determinado cliente, considere que sempre que for executar esta consulta será informado o IDCliente como 
 -- parâmetro. Deverão ser listados: Data do Pedido, Produto, Quantide, Valor Unitário, e valor total. 
 
+Select PE.DataPedido, PR.Nome, PI.Quantidade, PR.PrecoVenda "Valor unitáiro", (PR.PrecoVenda*PI.Quantidade) "Valor total" 
+from Cliente C 
+join Pedido PE on C.IdCliente = PE.IdCliente
+join PedidoItem PI on PE.IdPedido = PI.IdPedido
+join Produto PR on PI.IdProduto = PR.IdProduto;
 
 -- 7) Faça uma consulta que receba um parâmetro sendo o IDProduto e liste a quantidade de itens na tabela PedidoItem com este IDProduto foram 
 -- vendidos no último ano (desde janeiro/2016). 
