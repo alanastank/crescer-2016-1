5a0c18af36ddf15f54f2e32c8fd87647d85b19c0
Alana Stankiewicz <alanastankiewicz@gmail.com>
2016-06-10 17:32:35 -0300

    feat(Aula4): adicionar projeto da aula 4 com inicio do mapeamento Usuarios - Amigos - Publicações

---
+++ src/modulo-08-java/Aula4/src/main/java/br/com/cwi/aulas/aula4/Entity/Usuario.java
@@ -0,0 +1,48 @@
+package br.com.cwi.aulas.aula4.Entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Usuario implements Serializable {
+
+    @OneToMany(mappedBy = "Usuario", targetEntity = Publicacao.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Publicacao> publicacoes;
+
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_USUARIO")
+    @SequenceGenerator(name = "SEQ_USUARIO", sequenceName = "SEQ_USUARIO", allocationSize = 1)
+    @Basic(optional = false)
+    @Column(name = "ID_USUARIO")
+    public long idUsuario;
+
+    @Basic(optional = false)
+    @Column(name = "NM_USUARIO")
+    public String nomeUsuario;
+
+    public long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+}
